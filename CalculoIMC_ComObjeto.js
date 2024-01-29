41179f4f024d510ab1b6732d8b215e53d639f403
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2024-01-29 15:33:11 -0300

    Codigo refatorado com GPT

--- CalculoIMC_ComObjeto.js
+++ CalculoIMC_ComObjeto.js
@@ -5,48 +5,44 @@ Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e pe�
 do seu IMC; */
 
 class Pessoa {
-    nome;
-    peso;
-    altura;
-
-    constructor (nome, peso, altura) {
+    constructor(nome, peso, altura) {
         this.nome = nome;
         this.peso = peso;
         this.altura = altura;
-        
     }
 
-    calcularImc(){
+    calcularImc() {
         return this.peso / (this.altura * this.altura);
     }
-    classificarImc(){
+
+    classificarImc() {
         const imc = this.calcularImc();
-        if (imc < 18.5){
-            return ("O seu IMC é: " + imc.toFixed(2) + " Você está abaixo do peso.")
-        }
-        else if (imc >= 18.5 && imc < 25) {
-            return ("O seu IMC é: " + imc.toFixed(2) + " Você está com peso normal.")
-        }
-        else if (imc >= 25 && imc < 30) {
-            return ("O seu IMC é: " + imc.toFixed(2) + " você está acima do peso.")
-        }
-        else if (imc >= 30 && imc < 40) {
-            return ("O seu IMC é: " + imc.toFixed(2) + " você está obeso.")
-        }
-        else {
-            return ("O seu IMC é: " + imc.toFixed(2) + " Você se encontra em estado de \"Obesidade Grave.\"")
+        const mensagem = `O seu IMC é: ${imc.toFixed(2)}.`;
+
+        if (imc < 18.5) {
+            return `${mensagem} Você está abaixo do peso.`;
+        } else if (imc < 25) {
+            return `${mensagem} Você está com peso normal.`;
+        } else if (imc < 30) {
+            return `${mensagem} Você está acima do peso.`;
+        } else if (imc < 40) {
+            return `${mensagem} Você está obeso.`;
+        } else {
+            return `${mensagem} Você se encontra em estado de "Obesidade Grave."`;
         }
     }
 }
-const jose = new Pessoa ('José', 70, 1.75);
-const thiago = new Pessoa ('Thiago Souza', 91.5, 1.72);
-const juliana = new Pessoa ('Juliana', 73, 1.60);
+
+const jose = new Pessoa('José', 70, 1.75);
+const thiago = new Pessoa('Thiago Souza', 91.5, 1.72);
+const juliana = new Pessoa('Juliana', 73, 1.60);
 
 console.log(jose);
-console.log(jose.classificarImc())
+console.log(jose.classificarImc());
 
 console.log(thiago);
-console.log(thiago.classificarImc())
+console.log(thiago.classificarImc());
 
 console.log(juliana);
-console.log(juliana.classificarImc())
+console.log(juliana.classificarImc());
+
