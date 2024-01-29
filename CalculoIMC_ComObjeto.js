e0af7f7a87c792bd2d6297ea671744328ade98a1
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2024-01-29 15:26:42 -0300

    commit Cálculo de IMC com Objetos

--- CalculoIMC_ComObjeto.js
+++ CalculoIMC_ComObjeto.js
@@ -8,4 +8,45 @@ class Pessoa {
     nome;
     peso;
     altura;
+
+    constructor (nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+        
+    }
+
+    calcularImc(){
+        return this.peso / (this.altura * this.altura);
+    }
+    classificarImc(){
+        const imc = this.calcularImc();
+        if (imc < 18.5){
+            return ("O seu IMC é: " + imc.toFixed(2) + " Você está abaixo do peso.")
+        }
+        else if (imc >= 18.5 && imc < 25) {
+            return ("O seu IMC é: " + imc.toFixed(2) + " Você está com peso normal.")
+        }
+        else if (imc >= 25 && imc < 30) {
+            return ("O seu IMC é: " + imc.toFixed(2) + " você está acima do peso.")
+        }
+        else if (imc >= 30 && imc < 40) {
+            return ("O seu IMC é: " + imc.toFixed(2) + " você está obeso.")
+        }
+        else {
+            return ("O seu IMC é: " + imc.toFixed(2) + " Você se encontra em estado de \"Obesidade Grave.\"")
+        }
+    }
 }
+const jose = new Pessoa ('José', 70, 1.75);
+const thiago = new Pessoa ('Thiago Souza', 91.5, 1.72);
+const juliana = new Pessoa ('Juliana', 73, 1.60);
+
+console.log(jose);
+console.log(jose.classificarImc())
+
+console.log(thiago);
+console.log(thiago.classificarImc())
+
+console.log(juliana);
+console.log(juliana.classificarImc())
